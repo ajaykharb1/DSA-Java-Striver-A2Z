e03c7064b893c8293163eb1cc8b64f59656171a5
ajaykharb1 <ajaykharb232@gmail.com>
2026-09-22 12:14:33 +0530

    Solved half diamond pattern

--- 01-Basics/pattern1.java
+++ 01-Basics/pattern1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class patterns{
+public class pattern1{
     public static void main(String[] args) {
         System.out.print("Enter value of n: ");
         Scanner sc = new Scanner(System.in);
