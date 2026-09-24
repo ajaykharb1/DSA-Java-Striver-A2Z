77296690adad4cf4174bea77cd22a7c236ce6895
ajaykharb1 <ajaykharb232@gmail.com>
2026-09-24 23:56:27 +0530

    Solved Butterfly pattern

---
+++ 01-Basics/pattern3.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+public class pattern3{
+    public static void main(String[] args) {
+        System.out.print("Enter value of n: ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int spaces = 2*n-2;
+
+        for(int i = 1;i<=n;i++)
+        {
+            
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            for(int j=0;j<spaces;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+
+            spaces -= 2;
+
+            System.out.println("");
+        }
+
+        int space = 0;
+
+        for(int i = 1;i<=n;i++)
+        {
+            
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print("*");
+            }
+            for(int j=0;j<=space;j++)
+            {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n-i;j++)
+            {
+                System.out.print("*");
+            }
+
+            space += 2;
+
+            System.out.println("");
+        }
+
+    }
+}
